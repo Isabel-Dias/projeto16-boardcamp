c68fe4eda2eb1d3fd1328293f6e2884cb044809d
Isabel-Dias <isabel.c.andrade.dias@gmail.com>
2023-09-24 20:04:08 -0300

    Feat: post and get from routes games

--- src/controllers/games.controller.js
+++ src/controllers/games.controller.js
@@ -1,18 +1,58 @@
+import { gamesSchema } from "../schemas/games.schema.js";
+import db from "../database/database.js"
 
 async function getGames(req, res) {
     try {
-
+        const gamesList = await db.query(`
+            SELECT * FROM games;
+        `)
+        return res.status(200).send(gamesList.rows);
     } catch {
-
+        return res.sendStatus(500)
     }
 }
 
 async function postGames(req, res) {
     try {
+        const {name, image, stockTotal, pricePerDay} = req.body;
+        const validationSchema = gamesSchema.validate({name, image, stockTotal, pricePerDay});
+        
+        if(validationSchema.error){
+            return res.status(400).send("Todos os campos são obrigatórios")
+        }
+        const nameAlreadyExists = await db.query(
+            `SELECT name FROM games WHERE name = $1`, [name])
+
+         
+       if (nameAlreadyExists.rows.length>0) return res.sendStatus(409)
+        
+        await db.query(`
+            INSERT INTO games 
+                (name, image, "stockTotal", "pricePerDay") 
+            VALUES 
+                ($1, $2, $3, $4);`,[name, image, stockTotal, pricePerDay])
+
+        return res.sendStatus(201);
 
     } catch {
-
+        return res.sendStatus(500)
     }
 }
 
 export {getGames, postGames};
+
+let mock = [{
+    id: 1,
+    name: 'Banco Imobiliário',
+    image: 'http://',
+    stockTotal: 3,
+    pricePerDay: 1500,
+  },
+  {
+    id: 2,
+    name: 'Banco liário',
+    image: 'http://',
+    stockTotal: 2,
+    pricePerDay: 666,
+  }
+]
--- src/database/database.js
+++ src/database/database.js
@@ -1,3 +1,11 @@
-const connection = new Pool({
-    connectionString: process.env.DATABASE_URL,
-  });
+import pg from "pg";
+import dotenv from "dotenv";
+dotenv.config();
+
+const { Pool } = pg;
+
+const db = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+export default db;
