994eaedf973bea368be8199862a5fb8418c58536
Isabel-Dias <isabel.c.andrade.dias@gmail.com>
2023-09-25 14:09:23 -0300

    deploy config

--- src/database/database.js
+++ src/database/database.js
@@ -1,13 +1,13 @@
-import pg from "pg";
-import dotenv from "dotenv";
-dotenv.config();
+import pg from "pg"
+import dotenv from "dotenv"
+dotenv.config()
 
-const { Pool } = pg;
+const { Pool } = pg
 
-const db = new Pool({
+const configDatabase = {
   connectionString: process.env.DATABASE_URL,
-});
+};
 
 if (process.env.NODE_ENV === "production") configDatabase.ssl = true;
 
-export default db;
+export const db = new Pool(configDatabase);
